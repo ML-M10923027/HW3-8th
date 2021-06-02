b09accc89206db9a8d99bd8fb91a65aa49bda7e5
M10923027 <81971534+M10923027@users.noreply.github.com>
2021-06-03 01:23:39 +0800

    Add files via upload

---
+++ taiwan-high-speed-rail.php
@@ -0,0 +1,50 @@
+<?php
+$file_name = 'taiwan-high-speed-rail';
+
+$geometry = [
+    'type' => 'Point'
+];
+
+$lines = file($file_name . '.csv');
+foreach ($lines as $line) {
+    $stations[] = str_getcsv($line);
+}
+
+// remove column name
+unset($stations[0]);
+
+foreach ($stations as $station) {
+    list($id, $name, $zipcode, $address, $lat, $lon) = $station;
+
+    $geometry['coordinates'] = [(float) $lon, (float) $lat];
+    $properties = [
+        '編號' => (string) $id,
+        '站名' => $name,
+        '郵遞區號' => (int) $zipcode,
+        '地址' => $address,
+        '緯度' => (float) $lat,
+        '經度' => (float) $lon,
+        // https://help.github.com/en/github/managing-files-in-a-repository/mapping-geojson-files-on-github#styling-features
+        'marker-size' => 'medium',
+        'marker-symbol' => 'rail',
+        // use wikipedia color
+        'marker-color' => '#db5426',
+    ];
+
+    $feature = [
+        'type' => 'Feature',
+        'geometry' => $geometry,
+        'properties' => $properties
+    ];
+
+    $features[] = $feature;
+}
+
+$geojson = [
+    'type' => 'FeatureCollection',
+    'features' => $features
+];
+
+$handle = fopen($file_name . '.geojson', 'w+');
+fwrite($handle, json_encode($geojson, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+fclose($handle);
